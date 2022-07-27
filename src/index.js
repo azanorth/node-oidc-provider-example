23036c6e2d3c3117237ad817cf2642ca4b7fc68d
Pavlo Holovachov <adamdoom23@gmail.com>
2022-07-27 15:41:36 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -41,9 +41,9 @@ const oidc = new Provider(
           'https://bartlettgroup00.sandbox.myabsorb.com',
           'https://bartlettgroup.sandbox.myabsorb.com/api/rest/v2/authentication/openIdConnect',
         ], // using jwt.io as redirect_uri to show the ID Token contents
-        response_types: ['id_token', 'code'],
-        grant_types: ['implicit', 'authorization_code'],
-        token_endpoint_auth_method: 'none',
+        response_types: ['code'],
+        grant_types: ['authorization_code'],
+        token_endpoint_auth_method: 'client_secret_basic',
       },
     ],
     cookies: {
