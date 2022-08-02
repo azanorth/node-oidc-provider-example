4145fea0234217c17b4ef526715ae030d689749d
Pavlo Holovachov <adamdoom23@gmail.com>
2022-08-02 14:02:05 +0300

    Fixed path

--- src/index.js
+++ src/index.js
@@ -260,7 +260,7 @@ expressApp.use(oidc.callback());
 // express listen
 expressApp.listen(process.env.PORT);
 
-// https://obscure-oasis-20527.herokuapp.com/auth?client_id=foo&response_type=code&redirect_uri=https%3A%2F%2Fjwt.io&scope=openid%20email&nonce=foobar&state=af0ifjsldkj
+// https://obscure-oasis-20527.herokuapp.com/auth?client_id=foo&response_type=code&redirect_uri=https%3A%2F%2Fjwt.io&scope=openid%20email
 // https://obscure-oasis-20527.herokuapp.com/.well-known/openid-configuration
 // https://obscure-oasis-20527.herokuapp.com/jwks
 
