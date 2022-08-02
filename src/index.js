e3965f05d1ed82313f09ec1f93dd5275f12abb98
Pavlo Holovachov <adamdoom23@gmail.com>
2022-08-02 14:46:45 +0300

    Updates

--- src/index.js
+++ src/index.js
@@ -85,7 +85,11 @@ oidc.proxy = true;
 // let's work with express here, below is just the interaction definition
 const expressApp = express();
 
-expressApp.use('/interaction/*', express.static(`${__dirname}/public`));
+var public = path.join(__dirname, 'public');
+
+app.use('/', express.static(public));
+
+// expressApp.use('/interaction/*', express.static(`${__dirname}/public`));
 expressApp.set('trust proxy', true);
 expressApp.set('view engine', 'ejs');
 expressApp.set('views', path.resolve(__dirname, 'views'));
